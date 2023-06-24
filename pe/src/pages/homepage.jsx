74322e0361cb81d001fc9264e8e57a7c0bd5707f
rytherlm <aidanryther15@gmail.com>
2023-06-24 04:40:31 -0400

    Updates to the iamges just need to wokr on the test now nan it can be uplaod

--- pe/src/pages/homepage.jsx
+++ pe/src/pages/homepage.jsx
@@ -99,7 +99,7 @@ const Homepage = () => {
                 <div className="homepage-image-container">
                   <div className="homepage-image-wrapper">
                     <img
-                      src="homepage.jpg"
+                      src="homepage_new.jpeg"
                       alt="about"
                       className="homepage-image"
                     />
